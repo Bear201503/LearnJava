055b3957ece8c1820d998dbc2b6addda6cc2916e
esha <kramki10@yahoo.com>
2017-11-05 11:20:11 -0600

    refactoring

---
+++ src/whilepractice/WhilePractice.java
@@ -0,0 +1,12 @@
+package whilepractice;
+
+public class WhilePractice {
+
+    public static void main(String[] args) {
+
+        boolean checkUserInput = true;
+        while(checkUserInput){
+            System.out.println("Please enter a number");
+        }
+    }
+}
