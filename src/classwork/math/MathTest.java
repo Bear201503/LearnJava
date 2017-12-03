0210fcbe836ba869a988fa8d23f7f34e298d7416
esha <kramki10@yahoo.com>
2017-12-03 09:20:41 -0600

    tested math class

---
+++ src/classwork/math/MathTest.java
@@ -0,0 +1,15 @@
+package classwork.math;
+
+public class MathTest {
+
+    public static void main(String[] args) {
+
+        System.out.println((int)Math.sqrt(64));
+        int e = 5;
+        System.out.println((double)e);
+
+        System.out.println(Math.addExact(3,2));
+        System.out.println(Math.multiplyExact(4,5));
+        System.out.println(Math.subtractExact(10,4));
+    }
+}
